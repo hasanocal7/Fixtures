2c90091adc2d0c71734ea2b5ae6e950a97664e04
hasan <hasanocal7@gmail.com>
2023-10-24 15:40:55 +0300

    Update View

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
+require('dotenv').config();
 
 const app = express();
 
