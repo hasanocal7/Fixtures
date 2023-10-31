4a2ca61d6a652610d7c487664a6ac7cadc378e37
hasan <hasanocal7@gmail.com>
2023-10-31 12:31:03 +0300

    Added Create Furniture on Dashboard Page for Admin

--- controller/furnitureController.js
+++ controller/furnitureController.js
@@ -1,11 +1,24 @@
 const { Furniture } = require('../models');
+const multer = require('multer');
 
 exports.createFurniture = async (req, res) => {
   try {
-    const furniture = await Furniture.create(req.body);
-    res.status(201).json(furniture);
+    if (req.file instanceof multer.MulterError) {
+      res.status(400);
+      throw new Error('Resim yüklenirken Multer kaynaklı hata oluştu');
+    } else if (!req.file) {
+      res.status(400);
+      throw new Error({ error: 'Lütfen bir resim yükleyin' });
+    } else {
+      const imageFileName = '/uploads/' + req.file.filename;
+      const furniture = await Furniture.create({
+        ...req.body,
+        image: imageFileName,
+      });
+      res.status(201).redirect('/users/dashboard');
+    }
   } catch (error) {
-    res.status(400);
-    throw new Error('Something wrong');
+    console.error('Beklenmeyen bir hata oluştu:', error);
+    res.status(500).json({ error: 'Beklenmeyen bir hata oluştu' });
   }
 };
--- routes/furnitureRoute.js
+++ routes/furnitureRoute.js
@@ -1,6 +1,16 @@
 const router = require('express').Router();
 const furnitureController = require('../controller/furnitureController');
+const multer = require('multer');
+const upload = multer({ dest: './public/data/uploads/' });
 
-router.route('/addFurniture').post(furnitureController.createFurniture);
+router
+  .route('/addFurniture')
+  .post(upload.single('image'), furnitureController.createFurniture);
+
+router.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500);
+  throw new Error(err.message);
+});
 
 module.exports = router;
