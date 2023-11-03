c0dcd189c2c9440b0b304383379a1683d0ee4618
hasan <hasanocal7@gmail.com>
2023-11-03 14:12:46 +0300

    Edit

--- middleware/reserveMiddleware.js
+++ middleware/reserveMiddleware.js
@@ -1,24 +1,28 @@
 const { Furniture, Reserve } = require('../models');
 
 exports.checkReserv = async (req, res, next) => {
-  const furnitures = await Furniture.findAll({ where: { isReserved: true } });
-  for (let i = 0; i < furnitures.length; i++) {
-    const reserve = await Reserve.findOne({
-      where: { FurnitureId: furnitures[i].id },
-    });
-    if (furnitures[i].isReserved === true && reserve) {
-      continue;
-    } else if (furnitures[i].isReserved === true && !reserve) {
-      await Furniture.update(
-        {
-          isReserved: false,
-        },
-        { where: { id: furnitures[i].id } }
-      );
-    } else {
-      res.status(404);
-      throw new Error('Furniture and Reserve not found');
+  try {
+    const furnitures = await Furniture.findAll({ where: { isReserved: true } });
+    for (let i = 0; i < furnitures.length; i++) {
+      const reserve = await Reserve.findOne({
+        where: { FurnitureId: furnitures[i].id },
+      });
+      if (furnitures[i].isReserved && reserve) {
+        continue;
+      } else if (furnitures[i].isReserved && !reserve) {
+        await Furniture.update(
+          {
+            isReserved: false,
+          },
+          { where: { id: furnitures[i].id } }
+        );
+      } else {
+        res.status(404);
+        throw new Error('Furniture and Reserve not found');
+      }
     }
+    next();
+  } catch (error) {
+    next(error);
   }
-  next();
 };
